d8bef68db799a7d5ee28750a218682ed365136c7
MAIRE MAXIME <maxime.maire2@etu.unistra.fr>
2020-04-05 18:52:15 +0200

    jeu de la vie niveau 5

--- code/src/main.c
+++ code/src/main.c
@@ -4,6 +4,12 @@
 #include "io.h"
 #include "jeu.h"
 
+#ifdef MODE_GRA
+#include <cairo.h>
+#include <cairo-xlib.h>
+#include <X11/Xlib.h>
+#endif
+
 int main (int argc, char ** argv) {
 	
 	if (argc != 2 )
@@ -15,10 +21,14 @@ int main (int argc, char ** argv) {
 	grille g, gc;
 	init_grille_from_file(argv[1],&g);
 	alloue_grille (g.nbl, g.nbc, &gc);
-	affiche_grille(g);
 	
+	
+	#ifdef MODE_GRA
+	debut_jeu_cairo(&g,&gc);
+	#else
+	affiche_grille(g);
 	debut_jeu(&g, &gc);
-
+	#endif
 	libere_grille(&g);
 	libere_grille(&gc);
 	return 0;
